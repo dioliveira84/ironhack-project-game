a673509967f2ca187f6cded52748c726abbae369
Diogo Pires Oliveira <diogopiresoliveira@Diogos-MacBook-Air.local>
2019-03-19 16:34:49 -0300

    JS work in progress(answers fixed)

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -1,38 +1,36 @@
 // CONSTRUCTOR FUNCTION (QUESTIONS) //
-
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
+function Question(query, options, answer) {
+  this.query = query;
+  this.options = options;
   this.answer = answer;
 }
 
 // Correct Answer 
-Question.prototype.correctAnswer = function (choice) {
+Question.prototype.rightAnswer = function (choice) {
   return choice === this.answer;
 }
 
 // CONSTRUCTOR FUNCTION (TRIVIA) //
-
-function Quiz(questions) {
+function Trivia(questions) {
   this.score = 0;
   this.questions = questions;
   this.questionIndex = 0;
 }
 
 // Question Index
-Quiz.prototype.getQuestionIndex = function () {
+Trivia.prototype.getQuestionIndex = function () {
   return this.questions[this.questionIndex];
 }
 
 // Is End
-Quiz.prototype.isEnded = function () {
+Trivia.prototype.isEnded = function () {
   return this.questions.length === this.questionIndex;
 }
 
 // Score
-Quiz.prototype.guess = function (answer) {
-  this.questionIndex++;
-  if (this.getQuestionIndex().correctAnswer(answer)) {
+Trivia.prototype.guess = function (answer) {
+  if (this.getQuestionIndex().rightAnswer(answer)) {
     this.score++;
   }
+  this.questionIndex++;
 }
--- starter_code/js/questions.js
+++ starter_code/js/questions.js
@@ -1,22 +1,44 @@
-function populate() {
-  if (quiz.isEnded()) {
-    // showScores();
+function populateCard() {
+  if (trivia.isEnded()) {
+    displayScores();
   } else {
     // show question 
     var element = document.getElementById("cardtitle");
-    element.innerHTML = quiz.getQuestionIndex().text;
+    element.innerHTML = trivia.getQuestionIndex().query;
 
-    //show choices 
-    var choices = quiz.getQuestionIndex().choices;
-    for (var i = 0; i < choices.lenght; i++) {
-      var element = document.getElementById("choice"+i);
-      element.innerHTML = choices[i];
+    //show options 
+    var options = trivia.getQuestionIndex().options;
+    for (var i = 0; i < options.length; i++) {
+      var element = document.getElementById("choice" + i);
+      element.innerHTML = options[i];
+      guess("btn" + i, options[i]);
     }
+    displayProgress();
   }
+};
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function () {
+    trivia.guess(guess);
+    populateCard();
+  }
+};
+
+function displayProgress () {
+  var currentQuestionNumber = trivia.questionIndex + 1;
+  var element = document.getElementById("questions");
+  element.innerHTML = " Question " + currentQuestionNumber + " of " + trivia.questions.length;
 }
 
+function displayScores() {
+  var gameOverPage = "<h1>Result</h1>";
+  gameOverPage += "<h2 id='cardWrapper'>Your Score: " + trivia.score + "</h2>";
+  var element = document.getElementById("cardWrapper")
+  element.innerHTML = gameOverPage;
+};
 
-// Array of all the questions and choices to populate the questions.
+// Array of all the questions and options to populate the questions.
 var questions = [
   new Question("What course below is NOT and Ironhack Bootcamp?", ["Web Development", "Data Analytics", "Machine Learning", "UX / UI Design"], "Machine Learning"),
   new Question("IronHack foundation year", ["2013", "1994", "2009", "2000"], "2013"),
@@ -25,6 +47,6 @@ var questions = [
   new Question("This was a previous/temporary Ironhack campus in Sao Paulo-SP", ["Oficina Coworking", "Google Campus", "2WORK", "WeWork"], "WeWork")
 ];
 
-var quiz = new Quiz(questions);
+var trivia = new Trivia(questions);
 
-populate();
+populateCard();
